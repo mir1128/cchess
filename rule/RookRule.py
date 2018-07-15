0aeb47d9f697bdd80dbd5d9bdc22104fc99b1be2
liu jie <573240614@qq.com>
2018-07-16 01:06:27 +0800

    rook rule is ok. --liujie

--- rule/RookRule.py
+++ rule/RookRule.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from rule import Rule
+from Rule import Rule
 import constants
 
 
@@ -18,47 +18,67 @@ class RookRule(Rule):
         if (board[row_src][col_src] != constants.BLACK_ROOK) and (board[row_src][col_src] != constants.RED_ROOK):
             return False, False
 
+        # 起点和终点不在一条线上
         if (row_src != row_dst) and (col_src != col_dst):
             return False, False
 
+        # 在同一行
         if row_src == row_dst:
-            if any(v != 0 for v in board[row_src][col_src : col_dst]):
+            if any(v != 0 for v in board[row_src, min(col_src, col_dst) + 1, max(col_src, col_dst)].tolist()):
                 return False, False
 
+        # 在同一列
         if col_src == col_dst:
-            if any(v != 0 for v in board[row_src : row_dst][col_src]):
+            if any(v != 0 for v in board[min(row_src, row_dst) + 1: max(row_src, row_dst), col_src].tolist()):
                 return False, False
 
-        if board[row_src][col_src] < constants.BLACK_RED_LINE and board[row_dst][col_dst] > constants.BLACK_RED_LINE:
+        # 目标位置没有棋子
+        if board[row_src, col_src] < constants.BLACK_RED_LINE and board[row_dst, col_dst] == 0:
+            return True, False
+
+        # 红子吃掉黑子
+        if board[row_src, col_src] < constants.BLACK_RED_LINE < board[row_dst, col_dst]:
+            return True, super(RookRule, self).isOverAfterStep(dst, board)
+
+        # 黑子吃掉红子
+        if board[row_src, col_src] > constants.BLACK_RED_LINE > board[row_dst, col_dst]:
             return True, super(RookRule, self).isOverAfterStep(dst, board)
 
         return True, False
 
 
 if __name__ == '__main__':
+    import numpy as np
+
     r = RookRule()
-    s1 = [[1, 2, 3, 4, 7, 4, 3, 2, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 5, 0, 0, 0, 0, 0, 5, 0],
-         [6, 0, 6, 0, 6, 0, 6, 0, 6],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [16, 0, 16, 0, 16, 0, 16, 0, 16],
-         [0, 15, 0, 0, 0, 0, 0, 15, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [11, 12, 13, 14, 17, 14, 13, 12, 11]]
+    s1 = np.array([[1, 2, 3, 4, 7, 4, 3, 2, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 5, 0, 0, 0, 0, 0, 5, 0],
+                   [6, 0, 6, 0, 6, 0, 6, 0, 6],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [16, 0, 16, 0, 16, 0, 16, 0, 16],
+                   [0, 15, 0, 0, 0, 0, 0, 15, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [11, 12, 13, 14, 17, 14, 13, 12, 11]])
 
-    print(r.check((0, 0), (3, 0), s1))
+    try:
+        print(r.check((0, 0), (3, 0), s1))
+    except NameError, e:
+        print(e.message)
 
-    s2 = [[1, 2, 3, 4, 7, 4, 3, 2, 1],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 5, 0, 0, 0, 0, 0, 5, 0],
-          [0, 0, 6, 0, 6, 0, 6, 0, 6],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [16, 0, 16, 0, 16, 0, 16, 0, 16],
-          [0, 15, 0, 0, 0, 0, 0, 15, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [11, 12, 13, 14, 17, 14, 13, 12, 11]]
+    s2 = np.array([[1, 2, 3, 4, 7, 4, 3, 2, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 5, 0, 0, 0, 0, 0, 5, 0],
+                   [0, 0, 6, 0, 6, 0, 6, 0, 6],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [16, 0, 16, 0, 16, 0, 16, 0, 16],
+                   [0, 15, 0, 0, 0, 0, 0, 15, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [11, 12, 13, 14, 17, 14, 13, 12, 11]])
 
-    print(r.check((0, 0), (6, 0), s2))
+    try:
+        print(r.check((0, 0), (6, 0), s2))
+    except NameError, e:
+        print(e.message)
--- rule/Rule.py
+++ rule/Rule.py
@@ -15,11 +15,23 @@ class Rule(object):
 
     def check(self, src, dst, board):
         row_src, col_src = src
-        row_dst, row_dst = dst
+        row_dst, col_dst = dst
 
         if (not 0 <= row_src <= 10) or (not 0 <= col_src <= 9) or (not 0 <= row_dst <= 10) or (not 0 <= row_dst <= 9):
             logger.logger.error("invalid pos src %s, dst %s, board: %s", str(src), str(dst), str(board))
             raise NameError("index out of bound")
+
+        if board[row_src, col_src] == 0:
+            logger.logger.error("there is no chess at src: %s", str(src))
+            raise NameError("no chess found")
+
+        if board[row_src, col_src] < constants.BLACK_RED_LINE and board[row_dst, col_dst] != 0 and  board[row_dst, col_dst] < constants.BLACK_RED_LINE:
+            logger.logger.error("can not eat same side piece src: %s value %d, dst %s value %d", str(src), board[row_src, col_src], str(dst), board[row_dst, row_dst])
+            raise NameError("can't eat your own pieces")
+
+        if board[row_src, col_src] > constants.BLACK_RED_LINE and board[row_dst, col_dst] > constants.BLACK_RED_LINE:
+            logger.logger.error("can not eat same side piece src: %s value %d, dst %s value %d", str(src), board[row_src, col_src], str(dst), board[row_dst, row_dst])
+            raise NameError("can't eat your own pieces")
 
         return True, False
 

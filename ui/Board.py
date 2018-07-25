36f408a1954b3bc1a5e831fe55c5122812c3c4f0
liu jie <573240614@qq.com>
2018-07-25 23:56:37 +0800

    add rule to move method. --liujie

--- ui/Board.py
+++ ui/Board.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import crule
 
 class Board(object):
     __ROW = 10
@@ -9,26 +10,28 @@ class Board(object):
     def __init__(self, mode=0):
         if mode == 1:
             self.__board = np.array([[1, 2, 3, 4, 7, 4, 3, 2, 1],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 5, 0, 0, 0, 0, 0, 5, 0],
-                            [6, 0, 6, 0, 6, 0, 6, 0, 6],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [16, 0, 16, 0, 16, 0, 16, 0, 16],
-                            [0, 15, 0, 0, 0, 0, 0, 15, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [11, 12, 13, 14, 17, 14, 13, 12, 11]])
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 5, 0, 0, 0, 0, 0, 5, 0],
+                                     [6, 0, 6, 0, 6, 0, 6, 0, 6],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [16, 0, 16, 0, 16, 0, 16, 0, 16],
+                                     [0, 15, 0, 0, 0, 0, 0, 15, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [11, 12, 13, 14, 17, 14, 13, 12, 11]])
         elif mode == 0:
             self.__board = np.array([[11, 12, 13, 14, 17, 14, 13, 12, 11],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 15, 0, 0, 0, 0, 0, 15, 0],
-                            [16, 0, 16, 0, 16, 0, 16, 0, 16],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [6, 0, 6, 0, 6, 0, 6, 0, 6],
-                            [0, 5, 0, 0, 0, 0, 0, 5, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [1, 2, 3, 4, 7, 4, 3, 2, 1]])
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 15, 0, 0, 0, 0, 0, 15, 0],
+                                     [16, 0, 16, 0, 16, 0, 16, 0, 16],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [6, 0, 6, 0, 6, 0, 6, 0, 6],
+                                     [0, 5, 0, 0, 0, 0, 0, 5, 0],
+                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                     [1, 2, 3, 4, 7, 4, 3, 2, 1]])
+
+        self.__rules = [crule.CannonRule(), crule.ElephantRule(), crule.KingRule(), crule.KnightRule(), crule.MandarinRule(), crule.PawnRule(), crule.RookRule()]
 
     def update(self, board):
         self.__board = board
@@ -43,7 +46,17 @@ class Board(object):
                     env.putPiece(self.__board[row][col], (row, col))
 
     def move(self, c, src, dst):
-        row_src, col_src = src
-        row_dst, col_dst = dst
-        self.__board[row_dst, col_dst] = self.__board[row_src, col_src]
-        self.__board[row_src, col_src] = 0
+        isValid = False
+        isFihished = False
+        for v in self.__rules:
+            isValid, isFihished = v.check(src, dst, self.__board)
+            if isValid:
+                row_src, col_src = src
+                row_dst, col_dst = dst
+                self.__board[row_dst, col_dst] = self.__board[row_src, col_src]
+                self.__board[row_src, col_src] = 0
+                break
+        return isValid, isFihished
+
+if __name__ == '__main__':
+    print("hello world")
